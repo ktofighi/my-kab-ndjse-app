007204afade0734b7a0389bb4225ebbd36f93f51
Keyvan Tofighi <keyvan@tofighi.com>
2020-10-23 15:42:05 +0200

    V6

--- node.js.express2/app.js
+++ node.js.express2/app.js
@@ -5,7 +5,7 @@ module.exports = (/*options*/) => {
 
   app.get('/', (req, res) => {
     // Use req.log (a `pino` instance) to log JSON:
-    req.log.info({message: 'Hello from Home-again...!'});
+    req.log.info({message: 'Hello from Home-again... and again!'});
     res.send('Hello from Appsody!');
   });
 
